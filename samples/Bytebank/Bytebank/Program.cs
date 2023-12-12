c81ef7707e8e50d46cebc5fa1ecd08f543945cc8
hgrafa <hugorafell@gmail.com>
2023-12-12 20:17:54 -0300

    adding cadastro de pessoa

--- samples/Bytebank/Bytebank/Program.cs
+++ samples/Bytebank/Bytebank/Program.cs
@@ -5,11 +5,19 @@ namespace Bytebank; // file scoped namespace
 public class Program
 {
     public static Conta[] contas = new Conta[1000];
+    public static int size = 0;
+
+    // int a = 2, b = 2;
+
+    // Console.WriteLine(a++); // 2
+    // Console.WriteLine(a); // 3
+    // Console.WriteLine(++b); // 3
+    // Console.WriteLine(b); // 3
 
     // Ok: 1.1 criar uma funcao no qual gere um id aleatorio, que peca as informacoes de uma pessoa e
     // TODO: 1.2 salve em um array
     // TODO: 2. Listar todas as contas (nao vazias) presentes no array
-
+    // TODO: 3. Deletar uma conta pelo cpf ou apresentar mensagem caso nao exista
     
     public static void CadastrarPessoa() {
         Console.Write("Insira o nome: ");
@@ -23,29 +31,14 @@ public class Program
         int id = random.Next(1000, 9999);
 
         Conta contaParaCadastrar = Conta.FromPessoa(id, nome, cpf, idade);
+        contas[size++] = contaParaCadastrar;
     }
 
     public static void Main() // entrypoint
     {
-
-        // atributo estatico
-        double result = Circle.PI;
-
-        // Math.Log()
-        Circle.CalculateArea(3.0);
-
-        Pessoa joao = new Pessoa("Joao da Silva", "111.222.333-44", 23);
-        Conta c1 = new Conta(1, joao);
-
-        Conta c2 = new Conta(2, new Pessoa("Maria da Silva", "111.222.333-45", 26), 200);
-
-        c1.Depositar(300);
-
-        c1.Transferir(350, c2);
-
-        Console.WriteLine(c1);
-        Console.WriteLine("------------------------");
-        Console.WriteLine(c2);
+      CadastrarPessoa();
+      CadastrarPessoa();
+      CadastrarPessoa();
     }
 
 }
